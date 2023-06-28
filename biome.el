59fcb13df2b27fde04f2b23f5b9f146abd252e5b
SqrtMinusOne <thexcloud@gmail.com>
2023-06-28 15:38:38 +0300

    intital commit

---
+++ biome.el
@@ -0,0 +1,33 @@
+;;; biome.el --- Bountiful Interface to Open Meteo for Emacs  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Korytov Pavel
+
+;; Author: Korytov Pavel <thexcloud@gmail.com>
+;; Maintainer: Korytov Pavel <thexcloud@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+;; Homepage: https://github.com/SqrtMinusOne/biome
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;  TODO
+
+;;; Code:
+
+(provide 'biome)
+;;; biome.el ends here
